bcaa6098c4d8f29042bd721ee267d39abebb2124
keshav <ksv508@gmail.com>
2023-06-09 17:53:22 +0530

    first submit

---
+++ Main.java
@@ -0,0 +1,5 @@
+class main{
+    RWOnly rwonly = new RWOnly;
+    rwonly.name = "KESHAV";
+    System.out.println(rwonly.name);
+}
---
+++ RWOnly.java
@@ -0,0 +1,11 @@
+class RWOnly{
+    private string name;
+
+    public string getName() {
+        return name;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+}
