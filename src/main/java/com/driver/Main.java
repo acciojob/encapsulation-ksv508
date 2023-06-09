a3c8eaa429b3510f23623f08c180266d9e6988b4
keshav <ksv508@gmail.com>
2023-06-09 23:22:44 +0530

    Final Submit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        //obj.name = "Keshav"; //Make RWOnly private to public, so we use get and set
+        obj.setName("Keshav");
+        obj.getName();
+    }
 }
